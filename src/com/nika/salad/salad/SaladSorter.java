86237c28e2397b1f78008185db5af2701ed5c35e
nshkuratova <nika.shkuratova@gmail.com>
2017-03-04 13:57:19 +0300

    2

--- src/com/nika/salad/salad/SaladSorter.java
+++ src/com/nika/salad/salad/SaladSorter.java
@@ -3,6 +3,7 @@ package com.nika.salad.salad;
 import com.nika.salad.vegetable.Vegetable;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 /**
  * Created by nika.shkuratova on 04.03.2017.
@@ -16,5 +17,13 @@ public class SaladSorter {
 
     public Collection<Vegetable> sortByCalories() {
         return null;
+    }
+
+    private class CaloriesComparator implements Comparator<Vegetable> {
+
+        @Override
+        public int compare(Vegetable o1, Vegetable o2) {
+            return 0;
+        }
     }
 }
---
+++ src/com/nika/salad/salad/VegetableFinder.java
@@ -0,0 +1,30 @@
+package com.nika.salad.salad;
+
+import com.nika.salad.vegetable.Vegetable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by nika.shkuratova on 04.03.2017.
+ */
+public class VegetableFinder {
+    private final Salad salad;
+
+    public VegetableFinder(Salad salad) {
+        this.salad = salad;
+    }
+
+    public Collection<Vegetable> findVegetables(VegetableFilter vegetableFilter) {
+        Collection<Vegetable> vegetableCollection = new ArrayList<>();
+        if (vegetableFilter.isAccepted()) {
+            
+        }
+        return null;
+    }
+
+    public interface VegetableFilter {
+
+        boolean isAccepted(VegetablePortion vegetablePortion);
+    }
+}
