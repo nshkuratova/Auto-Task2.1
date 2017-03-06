e3ca00547da81777e989c029a1a670133a6d9dcc
nshkuratova <nika.shkuratova@gmail.com>
2017-03-06 15:25:53 +0300

    Project structure

--- src/com/nika/salad/Main.java
+++ src/com/nika/salad/Main.java
@@ -10,7 +10,6 @@ public class Main {
     public static void main(String[] args) {
 
         String chosenVegerables;
-        List<String> vegetablesList = new ArrayList<String>();
 
         System.out.println("Please choose vegetables for salad. Write down corresponding numbers separated by comas. E.g. 1,3,5 ");
         System.out.println("1. Carrot\n2. Broccoli");
@@ -19,12 +18,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         chosenVegerables = scanner.next();
 
-        for (int i = 0; i < chosenVegerables.length(); i++) {
-            vegetablesList.add(i, chosenVegerables.split(","));
-        }
+        String[] parts = chosenVegerables.split(",");
 
-        for (String str: vegetablesList) {
-
+        for (int i = 0; i < parts.length ; i++) {
+            System.out.println(parts[i]);
         }
 
     }
