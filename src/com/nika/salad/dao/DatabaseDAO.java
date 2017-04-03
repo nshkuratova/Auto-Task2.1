ad31573d8cd486ed058d687a50393af57ee9214e
nshkuratova <nika.shkuratova@gmail.com>
2017-04-03 12:32:47 +0300

    Javadoc for DatabaseDAO class

--- src/com/nika/salad/dao/DatabaseDAO.java
+++ src/com/nika/salad/dao/DatabaseDAO.java
@@ -8,20 +8,19 @@ import java.sql.*;
 
 /**
  * DatabaseDAO class is used to work with databases.
- * The class implements common interface
  * The main purpose of the class is to read data from the database and save data in the database.
  */
 public class DatabaseDAO extends BaseDAO {
     /**
      * This method is to be implemented.
-     * @param salad
+     * @param salad An instance of salad which will be saved in the database.
      */
     public void saveSalad(Salad salad) {
     }
 
     /**
-     *
-     * @return
+     * Method is used to retrieve data from the database and to return an instance of salad.
+     * @return an instance of salad (a list of vegetables and their weigh which are represented as vegetable portions)
      */
     public Salad readSalad() {
         Salad salad = new Salad();
