296d641e6c888ebd9e4a6513d2b6a9e46157f531
nshkuratova <nika.shkuratova@gmail.com>
2017-04-05 12:18:43 +0300

    Refactoring

--- src/com/nika/salad/salad/SaladSorter.java
+++ src/com/nika/salad/salad/SaladSorter.java
@@ -7,19 +7,32 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Created by nika.shkuratova on 04.03.2017.
+ * SaladSorter is a class used to perform sorting of vegetables in a salad by different criteria.
  */
 public class SaladSorter {
     private Salad salad;
 
-    public SaladSorter(Salad salad) {
+    /**
+     * Constructor with a parameter to create an object of SaladSorter class.
+     *
+     * @param salad an object of salad
+     * @throws NullPointerException if an empty salad (contains link which points to null) was passed to the constructor
+     */
+    public SaladSorter(Salad salad) throws NullPointerException {
         if (salad == null) {
             throw new NullPointerException("Salad is null!");
         }
         this.salad = salad;
     }
 
-    public List<Vegetable> sortBy(Comparator<Vegetable> vegetableComparator) {
+    /**
+     * Method used to sort vegetables by certain parameters and return a sorted list.
+     *
+     * @param vegetableComparator one of the classes implementing Comparator<Vegetable> interface is passed as a parameter depending on which type of sorting user has chosen
+     * @return a list of sorted vegetables according to the chosen by user type of sorting
+     * @throws NullPointerException if a vegetableComparator points at a link which contains null
+     */
+    public List<Vegetable> sortBy(Comparator<Vegetable> vegetableComparator) throws NullPointerException {
         if (vegetableComparator == null) {
             throw new NullPointerException("Sorting parameter is not chosen.");
         }
@@ -28,8 +41,24 @@ public class SaladSorter {
         return vegetables;
     }
 
+    /**
+     * Class which implements sorting by calories using Comparator<T> interface
+     *
+     * @see Comparator
+     */
     public static class CaloriesComparator implements Comparator<Vegetable> {
 
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         */
         @Override
         public int compare(Vegetable o1, Vegetable o2) {
             if (o1.getCalories() > o2.getCalories()) {
@@ -42,8 +71,24 @@ public class SaladSorter {
         }
     }
 
+    /**
+     * Class which implements sorting by proteins using Comparator<T> interface
+     *
+     * @see Comparator
+     */
     public static class ProteintComparator implements Comparator<Vegetable> {
 
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         */
         @Override
         public int compare(Vegetable o1, Vegetable o2) {
             if (o1.getProteins() > o2.getProteins()) {
@@ -56,8 +101,24 @@ public class SaladSorter {
         }
     }
 
+    /**
+     * Class which implements sorting by carbohydrates using Comparator<T> interface
+     *
+     * @see Comparator
+     */
     public static class CarbohydratesComparator implements Comparator<Vegetable> {
 
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         */
         @Override
         public int compare(Vegetable o1, Vegetable o2) {
             if (o1.getCarbohydrates() > o2.getCarbohydrates()) {
